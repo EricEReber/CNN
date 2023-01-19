2ce111147738d5f9340fd94d67153e7f43b681f5
Gregor Kajda <gregor.kajda@mindshareworld.com>
2023-01-19 17:40:17 +0100

    First version of _feedforward method finished for FullyConnectedLayer

--- src/Layers.py
+++ src/Layers.py
@@ -32,26 +32,56 @@ class Layer:
 
 class FullyConnectedLayer(Layer): 
     
-    def __init__(self, nodes, act_func: Callable, scheduler: Scheduler):
+    def __init__(self, nodes, act_func: Callable, scheduler: Scheduler, seed=None):
         self.nodes: list[int] = nodes
         self.act_func: Callable = act_func
         self.scheduler = scheduler
-        self.weights: np.ndarray = self._initialize_weights()
+        self.weights = self._initialize_weights()
+        self.seed = seed
 
-    def _initialize_weights(self):
-        
-        self.weights = np.random.rand(self.nodes[0]+1, self.nodes[1])
-    
-    def _reset_weights(self): 
-        
         self._initialize_weights()
 
+    def _initialize_weights(self):
+
+        if self.seed is not None: 
+            np.random.seed(self.seed)
+
+        weight_matrix = np.random.rand(self.nodes[0]+1, self.nodes[1])
+        
+        return weight_matrix
+
+    def _reset_weights(self): 
+        
+        if self.seed is not None: 
+            np.random.seed(self.seed)
+
+        self.weights = np.random.rand(self.nodes[0]+1, self.nodes[1])
+ 
     def _update_weights(self): 
         raise NotImplementedError
 
+    def _feedforward(self, X: np.ndarray):
         
+        if len(X.shape) == 1: 
+            X = X.reshape((1, X.shape[0]))
+
+        # Adding bias to the data
+        bias = np.ones(X.shape[0]) * 0.01
+        X = np.hstack([bias, X])
         
-        
+        self.z_matrix = np.zeros((X.shape[0], self.weights.shape[0]))
+        self.a_matrix = np.zeros((X.shape[0], self.weights.shape[0]))
+
+        for i in range(self.weights.shape[0]): 
+            z = X @ self.weights[i]
+            self.z_matrix[i, :] = z[:]  
+            a = self.act_func(z)
+            self.a_matrix[i, : ] = a[:]
+
+        return self.a_matrix
+
+    def _backpropagate(self):
+         
 
 class OutputLayer(FullyConnectedLayer):
 
@@ -67,3 +97,10 @@ class Pooling2DLayer(Layer):
 
     def __init__(self):
         super().__init__()
+
+
+A = np.array([1, 2, -2, 7])
+
+A = np.where(A > 0, A, 0)
+
+print(A)
