3884b5d0e2d82b086406aafa0bd2a6862fa42113
Gregor Kajda <gregor.kajda@mindshareworld.com>
2023-01-19 11:45:23 +0100

    CNN class file created as a copy of FFNN - Need some deletions and changes (On the case). Added a interface class called Layers

---
+++ src/Layers.py
@@ -0,0 +1,43 @@
+import math
+import autograd.numpy as np
+import sys
+
+"""
+Interface contatining all the layers that will be available for construction of 
+a models architecture.
+"""
+# TODO: Start implementation of FullyConnected, Output and Convolution Layer
+
+class Layer: 
+
+    def __init__(self): 
+        self
+    
+    def _feedforward(self): 
+        raise NotImplementedError 
+
+    def _backpropagate(self): 
+        raise NotImplementedError
+
+    def _reset_weights(self): 
+        raise NotImplementedError
+
+class FullyConnectedLayer(Layer): 
+    
+    def __init__(self): 
+        super().__init__()
+
+class OutputLayer(FullyConnectedLayer):
+
+    def __init__(self): 
+        super().__init__()
+
+class Convolution2DLayer(Layer): 
+
+    def __init__(self): 
+        super().__init__() 
+
+class Pooling2DLayer(Layer):
+
+    def __init__(self):
+        super().__init__()
