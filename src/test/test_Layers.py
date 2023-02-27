db49f247bf259440791e6d60a7435b1f0b48d2ba
Gregz9 <gregorkajda@gmail.com>
2023-02-27 11:11:25 +0100

    Test of FlattenLayer using an image from data-directory instead of breast-cancer data
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/test/test_Layers.py
+++ src/test/test_Layers.py
@@ -7,7 +7,9 @@ from src.FFNN import FFNN
 from src.CNN import CNN
 from src.costFunctions import CostOLS
 import autograd.numpy as np
-
+from src.Layers import FlattenLayer
+import matplotlib.pyplot as plt
+import imageio.v3 as imageio
 
 """
 Test file to test differnet layers and combinations of layers in a CNN 
@@ -42,8 +44,6 @@ print(f"{X_train.shape=}")
 H = X_train.shape[0]
 W = X_train.shape[1] // feature_maps
 
-print(W)
-
 reshaped_X_train = np.zeros((batches, feature_maps, H, W))
 index_counter = 0
 for b in range(batches):
@@ -51,6 +51,17 @@ for b in range(batches):
         for h in range(H):
             for w in range(W):
                 reshaped_X_train[b, fm, h, w] = X_train[h, w + fm * W]
+
+# Test of flatten layer using an image
+# img_path = "/home/gregz/Files/CNN/data/luna.JPG"
+# img = imageio.imread(img_path)
+#
+# batch = np.ndarray((1, img.shape[2], img.shape[0], img.shape[1]))
+# batch[0, :, :, :] = img[:, :, :].transpose(2, 0, 1)
+# print(f"{batch.shape=}")
+# flatten = FlattenLayer()
+# flat_img = flatten._feedforward(batch)
+# print(f"{flat_img.shape=}")
 
 print(f"{reshaped_X_train.shape=}")
 test_flatten = FlattenLayer(seed)
@@ -61,7 +72,8 @@ print(f"{flattened_reshaped.shape=}")
 adam_scheduler = Adam(eta, rho, rho2)
 momentum_scheduler = Momentum(eta, momentum)
 
-cnn = CNN(scheduler=adam_scheduler, seed=seed)
+
+# cnn = CNN(scheduler=adam_scheduler, seed=seed)
 
 # test way to connect layers
 print(f"{seed=}")
